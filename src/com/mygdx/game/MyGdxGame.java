26a589f94e5034b9bba280c02b1b3bb75b0b8961
MarkusSchulze <Markus-Schulze@gmx.net>
2014-12-11 13:24:13 +0100

    simple game for practise reasons

--- src/com/mygdx/game/MyGdxGame.java
+++ src/com/mygdx/game/MyGdxGame.java
@@ -2,26 +2,78 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private Texture dropImage;
+	private Texture bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+
+	private Rectangle bucket;
+	private Vector3 touchPos;
+
 	@Override
-	public void create () {
+	public void create() {
+		// load the images for the droplet and the bucket, 64x64 pixels each
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		// load the drop sound effect and the rain background "music"
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		// start the playback of the background music immediately
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		// Camera initiation
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		// Rechteck um den Eimer. Wird in der Mitte vom Screen erstellt mit 20
+		// Pixeln überhalb vom Boden
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
+
+		// Vector3 touchPos = new Vector3();
 	}
 
 	@Override
-	public void render () {
+	public void render() {
+		// Grundfarbe
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// Camera aktualisieren (Sollte jeden Frame gemacht werden)
+		camera.update();
+
+		// Eimer zeichnen
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(bucketImage, bucket.x, bucket.y);
 		batch.end();
+
+		// auf Touch reagieren
+		if (Gdx.input.isTouched()) {
+
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 64 / 2;
+		}
 	}
 }
