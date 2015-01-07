fb46a0c8e15610d69f5fafd70b73c718f5fb48c7
MarkusSchulze <Markus-Schulze@gmx.net>
2015-01-07 08:35:18 +0100

    new starting screen

---
+++ src/com/mygdx/game/MainScreen.java
@@ -0,0 +1,160 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+
+public class MainScreen implements Screen {
+
+	private Game game;
+	private Stage stage;
+	private Image imgC1;
+	private Image imgC2;
+	private Image imgC3;
+
+	public MainScreen(Game g) {
+		game = g;
+		float ScreenHeight = Gdx.graphics.getHeight();
+		float ScreenWidth = Gdx.graphics.getWidth();
+
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+
+		Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+		Texture texture = new Texture(Gdx.files.internal("StartScreen.jpg"));
+		Texture countdown1 = new Texture(Gdx.files.internal("countdown1.jpg"));
+		Texture countdown2 = new Texture(Gdx.files.internal("countdown2.jpg"));
+		Texture countdown3 = new Texture(Gdx.files.internal("countdown3.jpg"));
+
+		Image imgBackground = new Image(texture);
+		imgBackground.setPosition(0, 0);
+		imgBackground.setAlign(Align.bottomRight);
+		imgBackground.setHeight(Gdx.graphics.getHeight());
+		imgBackground.setWidth(Gdx.graphics.getWidth());
+
+		imgC1 = new Image(countdown1);
+		imgC1.setPosition(0, 0);
+		imgC1.setHeight(Gdx.graphics.getHeight());
+		imgC1.setWidth(Gdx.graphics.getWidth());
+		imgC1.setVisible(false);
+		imgC1.addListener(new ClickListener() {
+			@Override
+			public void touchUp(InputEvent e, float x, float y, int point, int button) {
+				game.setScreen(new SpellingGame());
+			}
+		});
+
+		imgC2 = new Image(countdown2);
+		imgC2.setPosition(0, 0);
+		imgC2.setHeight(Gdx.graphics.getHeight());
+		imgC2.setWidth(Gdx.graphics.getWidth());
+		imgC2.setVisible(false);
+		imgC2.addListener(new ClickListener() {
+			@Override
+			public void touchUp(InputEvent e, float x, float y, int point, int button) {
+				imgC1.setVisible(true);
+			}
+		});
+
+		imgC3 = new Image(countdown3);
+		imgC3.setPosition(0, 0);
+		imgC3.setHeight(Gdx.graphics.getHeight());
+		imgC3.setWidth(Gdx.graphics.getWidth());
+		imgC3.setVisible(false);
+		imgC3.addListener(new ClickListener() {
+			@Override
+			public void touchUp(InputEvent e, float x, float y, int point, int button) {
+				imgC2.setVisible(true);
+			}
+		});
+
+		TextButton btnStart = new TextButton("Start Game", skin);
+		btnStart.setPosition(ScreenWidth / 5, ScreenHeight / 2);
+		btnStart.setSize(ScreenWidth / 4, ScreenHeight / 10);
+		btnStart.addListener(new ClickListener() {
+			@Override
+			public void touchUp(InputEvent e, float x, float y, int point, int button) {
+				imgC3.setVisible(true);
+			}
+		});
+
+		TextButton btnOptions = new TextButton("Options", skin);
+		btnOptions.setPosition(ScreenWidth / 5, ScreenHeight / 8 * 3);
+		btnOptions.setSize(ScreenWidth / 4, ScreenHeight / 10);
+
+		TextButton btnScore = new TextButton("Score", skin);
+		btnScore.setPosition(ScreenWidth / 5, ScreenHeight / 4);
+		btnScore.setSize(ScreenWidth / 4, ScreenHeight / 10);
+
+		TextField txtHeadline = new TextField(" C ", skin);
+		txtHeadline.setPosition(100, 160);
+		txtHeadline.setSize(30, 30);
+		txtHeadline.setScale(50);
+		txtHeadline.setDisabled(true);
+
+		stage.addActor(imgBackground);
+		// stage.addActor(txtHeadline);
+		stage.addActor(btnScore);
+		stage.addActor(btnOptions);
+		stage.addActor(btnStart);
+		stage.addActor(imgC3);
+		stage.addActor(imgC2);
+		stage.addActor(imgC1);
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		stage.act();
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/mygdx/game/SpellingGame.java
@@ -0,0 +1,286 @@
+package com.mygdx.game;
+
+import java.util.Iterator;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.Sphere;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+public class SpellingGame extends ApplicationAdapter implements Screen {
+	private Texture dropImage;
+	private Texture bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+	private int numberOfLetters;
+	private String wordToSpell;
+	private BitmapFont font;
+
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+
+	private Sphere bucket;
+	private Sphere circle;
+	private Sphere circleCollision;
+	private Vector3 rotation;
+	private Vector3 touchPos;
+	Sphere spawningOuterSphere;
+	Sphere spawningInterSphere;
+
+	private Array<Sphere> raindrops;
+	private long lastDropTime;
+
+	private ShapeRenderer shaperenderer;
+	TextField letter;
+
+	// int viewportHeight;
+	// BitmapFont titleFont;
+	// BitmapFont textFont;
+	//
+	// private void createFonts() {
+	// FileHandle fontFile = Gdx.files.internal("data/Roboto-Bold.ttf");
+	// FreeTypeFontGenerator generator = new FreeTypeFontGenerator(fontFile);
+	// FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+	// parameter.size = 12;
+	// textFont = generator.generateFont(parameter);
+	// parameter.size = 24;
+	// titleFont = generator.generateFont(parameter);
+	// generator.dispose();
+	// }
+
+	@Override
+	public void create() {
+		Vector3 sphereCenter;
+		// TextureAtlas textureAtlas = new TextureAtlas("data/main");
+		// font = new BitmapFont(Gdx.files.internal("data/calibri.fnt"),
+		// textureAtlas.findRegion("calibri"), false);
+		// font = new BitmapFont();
+		// font.setColor(Color.RED);
+		// font.scale(20);
+
+		// createFonts();
+
+		Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+		letter = new TextField("test", skin);
+		letter.setPosition(100, 100);
+		numberOfLetters = 5;
+
+		// load the images for the droplet and the bucket, 64x64 pixels each
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		// load the drop sound effect and the rain background "music"
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		// start the playback of the background music immediately
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		// Camera initiation
+		camera = new OrthographicCamera();
+
+		System.out.println("Höhe: " + Gdx.graphics.getHeight());
+		System.out.println("Breite: " + Gdx.graphics.getWidth());
+		camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		batch = new SpriteBatch();
+
+		// Rechteck um den Eimer. Wird in der Mitte vom Screen erstellt mit 20
+		// Pixeln überhalb vom Boden
+		bucket = new Sphere(new Vector3(Gdx.graphics.getWidth() / 2 - 64 / 2, Gdx.graphics.getHeight() / 2 - 64, 0), 32);
+		/*
+		 * bucket.x = Gdx.graphics.getWidth() / 2 - 64 / 2; bucket.y = 20;
+		 * bucket.width = 64; bucket.height = 64;
+		 */
+		// Kreis auf dem die Kugel ballanciert wird. Es gibt einen Kreis der
+		// angezeigt wird und einen weiteren der bestimmt, wann die Kugel
+		// "runterfällt"
+		sphereCenter = new Vector3(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 0);
+
+		circleCollision = new Sphere(sphereCenter, Gdx.graphics.getHeight() / 2);
+		circle = new Sphere(sphereCenter, Gdx.graphics.getHeight() / 2 - bucket.radius);
+
+		raindrops = new Array<Sphere>();
+		spawnRaindrop();
+
+	}
+
+	// Funktion zum erstellen von random Tropfen in ein Array
+	private void spawnRaindrop() {
+		Vector3 sphereCenter;
+		float x = 0;
+		float y = 0;
+		boolean collision = false;
+
+		sphereCenter = new Vector3(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 0);
+
+		spawningOuterSphere = new Sphere(sphereCenter, Gdx.graphics.getHeight() / 2 - bucket.radius - 2 * 32);
+		spawningInterSphere = new Sphere(sphereCenter, 2 * bucket.radius + 32);
+
+		Iterator<Sphere> iter;
+		for (int i = 0; i < numberOfLetters; i++) {
+			x = MathUtils.random(Gdx.graphics.getWidth() / 2 - (Gdx.graphics.getHeight() / 2 - bucket.radius) + 32,
+					Gdx.graphics.getWidth() / 2 + (Gdx.graphics.getHeight() / 2 - bucket.radius) - 32);
+			y = MathUtils.random(bucket.radius, Gdx.graphics.getHeight() - bucket.radius);
+			Sphere raindrop = new Sphere(new Vector3(x, y, 0), 52);
+
+			collision = false;
+			iter = raindrops.iterator();
+			while (iter.hasNext()) {
+				Sphere testDummy = iter.next();
+				if (testDummy.overlaps(raindrop)) {
+					collision = true;
+				}
+			}
+			if (raindrop.overlaps(spawningOuterSphere) && !raindrop.overlaps(spawningInterSphere) && collision == false) {
+				raindrop.radius = 32;
+				raindrops.add(raindrop);
+			} else {
+				i--;
+			}
+
+		}
+		lastDropTime = TimeUtils.nanoTime();
+	}
+
+	@Override
+	public void render() {
+		// Grundfarbe
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// Camera aktualisieren (Sollte jeden Frame gemacht werden)
+		camera.update();
+
+		// Renderer für das Board
+		shaperenderer = new ShapeRenderer();
+		shaperenderer.setProjectionMatrix(camera.combined);
+		shaperenderer.begin(ShapeType.Filled);
+		shaperenderer.setColor(0, 1, 0, 1);
+		shaperenderer.circle(circle.center.x, circle.center.y, circle.radius);
+		// //DEBUG
+		// shaperenderer.setColor(0, 1, 1, 1);
+		// shaperenderer.circle(spawningOuterSphere.center.x,
+		// spawningOuterSphere.center.y, spawningOuterSphere.radius);
+		// shaperenderer.setColor(1, 1, 0, 1);
+		// shaperenderer.circle(spawningInterSphere.center.x,
+		// spawningInterSphere.center.y, spawningInterSphere.radius);
+		// shaperenderer.setColor(1, 1, 0, 1);
+		// shaperenderer.circle(bucket.center.x, bucket.center.y,
+		// bucket.radius);
+
+		shaperenderer.end();
+
+		if (!circleCollision.overlaps(bucket))
+			System.out.println("fail!!!!");
+
+		// auf Touch reagieren
+		if (Gdx.input.isTouched()) {
+			touchPos = new Vector3();
+
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.center.x = touchPos.x - 64 / 2;
+			bucket.center.y = 80;
+		}
+
+		// Bewegungssensor
+		rotation = new Vector3();
+		if (Gdx.input.getRoll() > 0) {
+			rotation.set(bucket.center.x, bucket.center.y++, 0);
+			camera.unproject(rotation);
+			bucket.center.y++;
+		} else {
+			rotation.set(bucket.center.x, bucket.center.y--, 0);
+			camera.unproject(rotation);
+			bucket.center.y--;
+		}
+		if (Gdx.input.getPitch() < 0) {
+			rotation.set(bucket.center.x++, bucket.center.y, 0);
+			camera.unproject(rotation);
+			bucket.center.x++;
+		} else {
+			rotation.set(bucket.center.x--, bucket.center.y, 0);
+			camera.unproject(rotation);
+			bucket.center.x--;
+		}
+		// // nach fester Zeit neuen Tropfen spawnen
+		// if (TimeUtils.nanoTime() - lastDropTime > 1000000000)
+		// spawnRaindrop();
+
+		// Tropfen Kollision
+		Iterator<Sphere> iter = raindrops.iterator();
+		while (iter.hasNext()) {
+			Sphere raindrop = iter.next();
+			if (raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+			}
+			// DEBUG
+			// shaperenderer.setProjectionMatrix(camera.combined);
+			// shaperenderer.begin(ShapeType.Filled);
+			// shaperenderer.setColor(1, 1, 0, 1);
+			// shaperenderer.circle(raindrop.center.x, raindrop.center.y,
+			// raindrop.radius);
+			// shaperenderer.end();
+
+			// raindrop.center.y -= 200 * Gdx.graphics.getDeltaTime();
+			// if (raindrop.center.y + 64 < 0)
+			// iter.remove();
+		}
+
+		// alles zeichnen mit der batch für openGL
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage, bucket.center.x - bucket.radius, bucket.center.y - bucket.radius);
+		for (Sphere raindrop : raindrops) {
+			batch.draw(dropImage, raindrop.center.x - raindrop.radius, raindrop.center.y - raindrop.radius);
+		}
+		// font.draw(batch, "C", 200, 200);
+		// letter.draw(batch, 0);
+		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
+		batch.dispose();
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
